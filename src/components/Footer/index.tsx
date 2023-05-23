aa75da72bc143684e858121c994dd3f134bc6272
Roberto Marcos <roberdm49@gmail.com>
2023-05-23 15:03:27 -0300

    fix footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -3,9 +3,9 @@ import styles from './styles.module.scss'
 export const Footer = () => (
   <footer className={styles.container}>
     <div className={styles['inner-wrapper']}>
-      Made with ❤ by <a href='https://www.linkedin.com/in/roberto-david-marcos/' target='_blank'> Roberto Marcos </a>
+      Made with <span className={styles.highlight}>❤</span> by <a className={styles.highlight} href='https://www.linkedin.com/in/roberto-david-marcos/' target='_blank'> Roberto Marcos </a>
       {' | '}
-      <a href="https://icons8.com/" target='_blank'>Icons made by Icons8</a>
+      Icons made <a className={styles.highlight} href="https://icons8.com/" target='_blank'>by Icons8</a>
     </div>
   </footer>
 )
