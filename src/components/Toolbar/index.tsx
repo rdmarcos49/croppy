6c213234409e9c97dad7a83ec48210fbcd64ece2
Roberto Marcos <roberdm49@gmail.com>
2023-06-07 18:41:17 -0300

    refactor position abs

--- src/components/Toolbar/index.tsx
+++ src/components/Toolbar/index.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef, useState } from 'react'
 import CutIcon from '../../assets/cut.png'
 import EraseIcon from '../../assets/erase.png'
 import MoveIcon from '../../assets/move.png'
@@ -6,28 +7,104 @@ import RedoIcon from '../../assets/redo.png'
 import UndoIcon from '../../assets/Undo.png'
 import styles from './styles.module.scss'
 
-export const Toolbar = () => (
-  <div className={`${styles.container} with-box-s`}>
-    <button className={`${styles.action} ${styles['action--disabled']}`} disabled>
-      <img className={styles.icon} alt='undo' src={UndoIcon} />
-    </button>
-    <button className={`${styles.action} ${styles['action--disabled']}`} disabled>
-      <img className={styles.icon} alt='redo' src={RedoIcon} />
-    </button>
+type Coordinate = {
+  x: number,
+  y: number
+}
 
-    <span className={styles.separator} />
+export const Toolbar = () => {
+  const containerRef = useRef<HTMLElement | null>(null)
 
-    <button className={styles.action}>
-      <img className={styles.icon} alt='cut' src={CutIcon} />
-    </button>
-    <button className={styles.action}>
-      <img className={styles.icon} alt='erase' src={EraseIcon} />
-    </button>
-    <button className={styles.action}>
-      <img className={styles.icon} alt='move' src={MoveIcon} />
-    </button>
-    <button className={styles.action}>
-      <img className={styles.icon} alt='pen' src={PenIcon} />
-    </button>
-  </div>
-)
+  useEffect(() => {
+    if (!containerRef.current) return
+
+    let isMoving = false
+    let mousePosition: Coordinate = {
+      x: 0,
+      y: 0
+    }
+
+    const handleMouseDown = (event) => {
+      if (event.target.nodeName !== 'SECTION') return
+
+      mousePosition = {
+        x: event.clientX,
+        y: event.clientY
+      }
+      isMoving = true
+    }
+
+    const handleMouseMove = (event) => {
+      if (!isMoving) return
+      if (!containerRef.current) return
+
+      const { offsetLeft, offsetTop } = event.target
+
+      const moveValueX = event.clientX > mousePosition.x ? Math.abs(event.clientX - mousePosition.x) : -Math.abs(event.clientX - mousePosition.x)
+      const moveValueY = event.clientY > mousePosition.y ? Math.abs(event.clientY - mousePosition.y) : -Math.abs(event.clientY - mousePosition.y)
+
+      mousePosition = {
+        x: event.clientX,
+        y: event.clientY
+      }
+
+      const newContainerPositionX = offsetLeft + moveValueX
+      const newContainerPositionY = offsetTop + moveValueY
+
+      containerRef.current.style.left = `${newContainerPositionX}px`
+      containerRef.current.style.top = `${newContainerPositionY}px`
+    }
+
+    const handleOnMouseUp = () => {
+      isMoving = false
+    }
+
+    const handleMouseLeave = (event) => {
+      isMoving = false
+      mousePosition = {
+        x: 0,
+        y: 0
+      }
+    }
+
+    containerRef.current.addEventListener('mousedown', handleMouseDown)
+    containerRef.current.addEventListener('mouseup', handleOnMouseUp)
+    containerRef.current.addEventListener('mousemove', handleMouseMove)
+
+    return () => {
+      containerRef.current?.removeEventListener('mousedown', handleMouseDown)
+      containerRef.current?.removeEventListener('mouseup', handleOnMouseUp)
+      containerRef.current?.removeEventListener('mousemove', handleMouseMove)
+      containerRef.current?.removeEventListener('mouseleave', handleMouseLeave)
+    }
+  }, [])
+
+  return (
+    <section
+      ref={containerRef}
+      className={`${styles.container} with-box-s`}
+    >
+      <button className={`${styles.action} ${styles['action--disabled']}`} disabled>
+        <img className={styles.icon} alt='undo' src={UndoIcon} />
+      </button>
+      <button className={`${styles.action} ${styles['action--disabled']}`} disabled>
+        <img className={styles.icon} alt='redo' src={RedoIcon} />
+      </button>
+
+      <span className={styles.separator} />
+
+      <button className={styles.action}>
+        <img className={styles.icon} alt='cut' src={CutIcon} />
+      </button>
+      <button className={styles.action}>
+        <img className={styles.icon} alt='erase' src={EraseIcon} />
+      </button>
+      <button className={styles.action}>
+        <img className={styles.icon} alt='move' src={MoveIcon} />
+      </button>
+      <button className={styles.action}>
+        <img className={styles.icon} alt='pen' src={PenIcon} />
+      </button>
+    </section>
+  )
+}
